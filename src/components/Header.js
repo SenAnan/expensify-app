f03cfe4ce1bf7e3653a578593a0daad62886401a
Sen Ananthasayanan <s.ananthasayanan@yahoo.co.uk>
2020-06-30 21:38:28 +0100

    Added user auth

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,34 @@
 import React from 'react';
-import {NavLink} from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { startLogout } from '../actions/auth';
 
-const Header = () => (
-    <header>
-        <h1>Expensify</h1>
-        <ul>
-            <li><NavLink to="/" activeClassName="is-active" exact={true}>Dashboard</NavLink></li>
-            <li><NavLink to="/create" activeClassName="is-active">Add Expense</NavLink></li>
-            <li><NavLink to="/help" activeClassName="is-active">Help</NavLink></li>
-        </ul>
-    </header>
-)
+const Header = (props) => (
+	<header>
+		<h1>Expensify</h1>
+		<ul>
+			<li>
+				<NavLink to="/dashboard" activeClassName="is-active">
+					Dashboard
+				</NavLink>
+			</li>
+			<li>
+				<NavLink to="/create" activeClassName="is-active">
+					Add Expense
+				</NavLink>
+			</li>
+			<li>
+				<NavLink to="/help" activeClassName="is-active">
+					Help
+				</NavLink>
+			</li>
+			<button onClick={props.startLogout}>Logout</button>
+		</ul>
+	</header>
+);
 
-export default Header;
+const mapDispatchToProps = (dispatch) => ({
+	startLogout: () => dispatch(startLogout()),
+});
+
+export default connect(undefined, mapDispatchToProps)(Header);
